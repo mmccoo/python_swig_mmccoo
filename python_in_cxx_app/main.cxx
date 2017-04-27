10c57e6d295146a0aac937d8f758340dd173be8e
Miles McCoo <mail@mmccoo.com>
2017-04-27 19:53:55 +0200

    updated makefiles. fixed py syntax error

--- python_in_cxx_app/main.cxx
+++ python_in_cxx_app/main.cxx
@@ -3,16 +3,27 @@
 
 #include <Python.h>
 
+static wchar_t* charToWChar(const char* text)
+{
+    const size_t size = strlen(text) + 1;
+    wchar_t* wText = new wchar_t[size];
+    mbstowcs(wText, text, size);
+    return wText;
+}
+
+
+
+
 int
 main(int argc, char *argv[])
 {
     // if you get an error complaining about not being able to convert
     // char* to wchar*, you're missing some compile flags. python-config --cflags
     // is your friend
-    Py_SetProgramName(argv[0]);  /* optional but recommended */
+    Py_SetProgramName(charToWChar(argv[0]));  /* optional but recommended */
     Py_Initialize();
     PyRun_SimpleString("from time import time,ctime\n"
-                       "print 'Today is',ctime(time())\n");
+                       "print('Today is',ctime(time()))\n");
     Py_Finalize();
     return 0;
 }
